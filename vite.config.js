8e5f59fbf57770dac21328203568830854ed31db
HuangChao <2170189105@qq.com>
2026-04-07 15:34:19 +0800

    update vite.config.js: add allowedHosts

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,7 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5173,
+    allowedHosts: ['frp.dtu2.xyz'],
     proxy: {
       '/api': {
         target: 'http://127.0.0.1:8090',
